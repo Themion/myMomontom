559882996c9d0e6da4ff928d20befc4d409213fe
Themion <themion@naver.com>
2020-10-17 21:35:34 +0900

    Add files via upload

---
+++ src/bg.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body")
+
+const IMAGE = 8
+
+//백그라운드 이미지의 번호를 받아 백그라운드를 설정
+function set_bg (num) {
+    //이미지 객체를 생성한 뒤 이미지와 클래스를 지정
+    const image = new Image()
+    image.src = `./src/images/${num}.jpg`
+    image.classList.add('bg')
+
+    //페이지의 백그라운드로 추가
+    body.appendChild(image)
+}
+
+//images 폴더 안의 랜덤한 이미지를 백그라운드로 설정
+function init () {
+    set_bg(Math.floor(Math.random() * IMAGE))
+}
+
+init()
